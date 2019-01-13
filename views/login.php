a5673485b80b33fa8b51c537042a4dfa5b80763d
Thiago Benites <thi.fam2014@gmail.com>
2019-01-13 09:37:25 -0200

    Página de login 1

---
+++ views/login.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="description" content="Sistema de Prontuário Médico" >
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+        <title>Pronturário Online</title>
+    </head>
+
+    <body>
+        <nav class="navbar bg-dark">
+            <p>Prontuário Online</p>
+            <form class="form-inline">
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" placeholder="Usuário">
+                    <input class="form-control" type="password" placeholder="Senha">
+                    <div class="input-group-append"><button class="btn btn-outline-light">Login</button></div>
+                </div>
+            </form>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-9"></div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <script src="../js/bootstrap.js" type="text/javascript" charset="utf-8" async defer></script>
+    </body>
+</html>

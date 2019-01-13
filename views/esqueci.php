d9e86d62d4ea9fb5d8fcdea0e4295068951f1585
Thiago Benites <thi.fam2014@gmail.com>
2019-01-13 12:52:40 -0200

    Tela esqueci senha 1

--- views/esqueci.php
+++ views/esqueci.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <!--<link rel="stylesheet" href="../css/all.css">-->
 
-        <title>Pronturário Online - Login</title>
+        <title>Pronturário Online - Esqueci a senha</title>
     </head>
 
     <body>
@@ -20,7 +20,7 @@
             <div class="d-flex justify-content-center h-100">
                 <div class="card">
                     <div class="card-header">
-                        <h3>Login</h3>
+                        <h5>Recuperar dados de acesso</h5>
                         <!--<div class="d-flex justify-content-end social_icon">
                             <span><i class="fab fa-facebook-square"></i></span>
                             <span><i class="fab fa-google-plus-square"></i></span>
@@ -31,29 +31,24 @@
                         <form>
                             <div class="input-group form-group">
                                 <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <span class="input-group-text"><i class="fas fa-mail-bulk"></i></span>
                                 </div>
-                                <input type="text" class="form-control" placeholder="usuário">
+                                <input type="text" class="form-control" placeholder="e-mail">
                                 
                             </div>
-                            <div class="input-group form-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
-                                </div>
-                                <input type="password" class="form-control" placeholder="senha">
+                            <div class="container-radio">
+                                <input type="radio" checked="checked">Senha
+                                <input type="radio">Usuário
                             </div>
-                            <!--<div class="row align-items-center remember">
-                                <input type="checkbox">Remember Me
-                            </div>-->
                             <div class="form-group">
-                                <input type="submit" value="Entrar" class="btn float-right login_btn">
+                                <input type="submit" value="Enviar" class="btn float-right login_btn">
                             </div>
                         </form>
                     </div>
                     <div class="card-footer">
-                        <div class="d-flex justify-content-center">
-                            <a href="esqueci.php">Esqueceu sua senha?</a>
-                        </div>
+                        <!--<div class="d-flex justify-content-center">
+                            <a href="#">Esqueceu sua senha?</a>
+                        </div>-->
                     </div>
                 </div>
             </div>

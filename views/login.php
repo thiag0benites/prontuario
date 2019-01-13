7d79a702e341f9246337ae0765e711b17847d176
Thiago Benites <thi.fam2014@gmail.com>
2019-01-13 09:51:26 -0200

    Página de login 2

--- views/login.php
+++ views/login.php
@@ -8,29 +8,41 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/custom.css">
 
         <title>Pronturário Online</title>
     </head>
 
     <body>
-        <nav class="navbar bg-dark">
-            <p>Prontuário Online</p>
-            <form class="form-inline">
-                <div class="input-group mb-3">
-                    <input class="form-control" type="text" placeholder="Usuário">
-                    <input class="form-control" type="password" placeholder="Senha">
-                    <div class="input-group-append"><button class="btn btn-outline-light">Login</button></div>
-                </div>
-            </form>
-        </nav>
         <div class="container">
-            <div class="row">
-                <div class="col-9"></div>
-                <div class="col-3"></div>
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <!-- Tabs Titles -->
+
+                    <!-- Icon -->
+                    <div class="fadeIn first">
+                    <img src="https://www.b-cube.in/wp-content/uploads/2014/05/aditya-300x177.jpg" id="icon" alt="User Icon" />
+                    <h1>Aditya News</h1>
+                    </div>
+
+                    <!-- Login Form -->
+                    <form>
+                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="username">
+                    <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+                    <input type="submit" class="fadeIn fourth" value="Log In">
+                    </form>
+
+                    <!-- Remind Passowrd -->
+                    <div id="formFooter">
+                    <a class="underlineHover" href="#">Go to the Site</a>
+                    </div>
+
+                </div>
             </div>
         </div>
 
         <!-- Optional JavaScript -->
-        <script src="../js/bootstrap.js" type="text/javascript" charset="utf-8" async defer></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+        <script src="../js/jquery.min.js"></script>
     </body>
 </html>
